71d9f445a3d36adac9385957059dae2f69665f5b
Giovanna Tintori <giovannatintori@hotmail.com>
2023-12-18 17:24:17 -0300

    Level two

---
+++ levelTwo.js
@@ -0,0 +1,9 @@
+const inbox = ['A', 'B', 'C', 'D', 'E'];
+const outbox = [];
+
+while (inbox.length > 0) {
+    const currentBox = inbox.shift();
+    outbox.push(currentBox);
+    console.log('Movendo caixa:', currentBox);
+}
+console.log('Outbox:', outbox);
